204af6f539b7fc7cb82d960b8919c6eae4874094
michaelk0217 <temin.kim@gmail.com>
2024-10-02 14:20:47 -0700

    landing ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,30 @@
+import ClientSection from "@/components/landing-page/landing/client-section";
+import CallToActionSection from "@/components/landing-page/landing/cta-section";
+import HeroSection from "@/components/landing-page/landing/hero-section";
+import PricingSection from "@/components/landing-page/landing/pricing-section";
+import Particles from "@/components/landing-page/magicui/particles";
+import { SphereMask } from "@/components/landing-page/magicui/sphere-mask";
+import { SiteHeader } from "@/components/landing-page/site-header";
+import { SiteFooter } from "@/components/landing-page/site-footer";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <SiteHeader />
+      <HeroSection />
+      <ClientSection />
+      <SphereMask />
+      <PricingSection />
+      <CallToActionSection />
+      <Particles
+        className="absolute inset-0 -z-10"
+        quantity={50}
+        ease={70}
+        size={0.05}
+        staticity={40}
+        color={"#ffffff"}
+      />
+      <SiteFooter />
+    </>
+  );
 }
